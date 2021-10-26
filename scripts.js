806079cf2655be3eddf5ff6976bff860065c6688
RHBusch <91342001+RHBusch@users.noreply.github.com>
2021-10-25 20:44:56 -0400

    Adding alert to list

--- scripts.js
+++ scripts.js
@@ -4,3 +4,9 @@ the input*/
 let li = $('<li></li>');
    let inputValue = $('#input').val();
    li.append(inputValue);
+
+   if (inputValue === '') {
+        alert("You must write something!");
+      } else {
+        $('#list').append(li)
+      }
